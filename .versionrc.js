668da1f9c7188cdd33609e1e4a170ceea731566e
brunoreinstein-cloud <bruno.reinstein@gmail.com>
2025-09-12 08:21:52 -0300

    chore: add standard-version release tooling

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'conventionalcommits',
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'docs', section: 'Documentation' },
+    { type: 'style', section: 'Styles' },
+    { type: 'refactor', section: 'Refactoring' },
+    { type: 'test', section: 'Tests' },
+    { type: 'chore', section: 'Chores' },
+    { type: 'security', section: 'Security' },
+    { type: 'perf', section: 'Performance' }
+  ]
+};
