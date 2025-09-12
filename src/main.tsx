3696c16ccf32be4948e36fb82d6e0c63f35f4a14
brunoreinstein-cloud <bruno.reinstein@gmail.com>
2025-09-12 17:58:28 -0300

    feat: add consent utilities

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import './styles/assistjur-brand.css'
 import { MaintenanceProvider } from '@/hooks/useMaintenance'
 import { getEnv } from '@/lib/getEnv'
+import { getConsent, onConsentChange } from '@/lib/consent'
 
 const { sentryDsn } = getEnv()
 
@@ -23,10 +24,39 @@ if (import.meta.env.DEV) {
   ;(globalThis as any).posthog?.opt_out_capturing?.()
 }
 
-createRoot(document.getElementById("root")!).render(
+let analyticsScript: HTMLScriptElement | null = null
+
+function loadAnalytics() {
+  if (analyticsScript) return
+  analyticsScript = document.createElement('script')
+  analyticsScript.src = 'https://assistjur.com.br/~flock.js'
+  analyticsScript.defer = true
+  analyticsScript.id = 'aj-flock'
+  document.head.appendChild(analyticsScript)
+}
+
+function unloadAnalytics() {
+  analyticsScript?.remove()
+  analyticsScript = null
+}
+
+const consent = getConsent()
+if (consent.measure) {
+  loadAnalytics()
+}
+
+onConsentChange((c) => {
+  if (c.measure) {
+    loadAnalytics()
+  } else {
+    unloadAnalytics()
+  }
+})
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MaintenanceProvider>
       <App />
     </MaintenanceProvider>
   </StrictMode>
-);
+)
